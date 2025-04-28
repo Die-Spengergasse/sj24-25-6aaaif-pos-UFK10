9ac89309acec06a21acb86764590772df2500601
Umut Faiz Karagöz <KAR20286@spengergasse.at>
2025-04-28 17:44:15 +0200

    Übung Nummer 6

--- Asp_Wiederholung_6AAIF/src/SPG_Fachtheorie.Aufgabe1/Model/Manager.cs
+++ Asp_Wiederholung_6AAIF/src/SPG_Fachtheorie.Aufgabe1/Model/Manager.cs
@@ -1,19 +1,28 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace SPG_Fachtheorie.Aufgabe1.Model
 {
     public class Manager : Employee
     {
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+#pragma warning disable CS8618
         protected Manager() { }
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+#pragma warning restore CS8618
+
         public Manager(int registrationNumber, string firstName, string lastName,
-            Address? address, string carType) : base(registrationNumber, firstName, lastName, address)
+      Address? address, string carType, DateTime? lastUpdate = null, string? department = null)
+      : base(registrationNumber, firstName, lastName, address)
         {
             CarType = carType;
+            LastUpdate = lastUpdate ?? DateTime.UtcNow;
+            Department = department;
         }
+
 
         [MaxLength(255)]
         public string CarType { get; set; }
+
+        public DateTime? LastUpdate { get; set; }
+        public string? Department { get; set; }
     }
 }
